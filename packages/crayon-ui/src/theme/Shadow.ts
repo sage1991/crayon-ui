6305067406ef037907f579a3e185279b9e7b390a
Harry <harry.kane@kakaostyle.com>
2022-09-13 00:08:51 +0900

    Chage shadow

--- packages/crayon-ui/src/theme/Shadow.ts
+++ packages/crayon-ui/src/theme/Shadow.ts
@@ -1,5 +1,5 @@
 export namespace Shadow {
-  export const low = "0 4px 8px rgba(0, 0, 0, 0.4)"
-  export const medium = "0 8px 16px rgba(0, 0, 0, 0.4)"
-  export const high = "0 12px 24px rgba(0, 0, 0, 0.4)"
+  export const low = "0 4px 4px rgba(0, 0, 0, 0.4)"
+  export const medium = "0 8px 8px rgba(0, 0, 0, 0.4)"
+  export const high = "0 12px 12px rgba(0, 0, 0, 0.4)"
 }
