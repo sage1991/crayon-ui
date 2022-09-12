7341a8ebbffb60617412c3016bc785fef1b5341b
Harry <harry.kane@kakaostyle.com>
2022-09-12 22:41:48 +0900

    Declare theme type

---
+++ packages/crayon-ui/src/@types/emotion.d.ts
@@ -0,0 +1,31 @@
+import { CSSProperties } from "react"
+
+declare module "@emotion/react" {
+  type ColorVariant = "primary" | "secondary" | "warning" | "error" | "success" | "text"
+
+  interface ColorTheme {
+    main: string
+    contrast: string
+  }
+
+  type PaletteTheme = {
+    [K in ColorVariant]: ColorTheme
+  }
+
+  interface TypographyTheme {
+    font: CSSProperties["fontFamily"]
+    size: CSSProperties["fontSize"]
+  }
+
+  interface Theme {
+    palette: PaletteTheme
+    typography: TypographyTheme
+    zIndex: {
+      fab: number
+      drawer: number
+      modal: number
+      snackbar: number
+      tooltip: number
+    }
+  }
+}
