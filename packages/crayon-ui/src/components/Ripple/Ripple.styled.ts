a2dfacea5620dee393dc61e83064f65227abe94d
Harry <harry.kane@kakaostyle.com>
2022-09-19 11:44:45 +0900

    Improve ripple interaction
    Ripple effect will not be disappear while pointer is down

--- packages/crayon-ui/src/components/Ripple/Ripple.styled.ts
+++ packages/crayon-ui/src/components/Ripple/Ripple.styled.ts
@@ -7,6 +7,7 @@ import { Mixin } from "../../theme"
 export const RippleRoot = styled("span")`
   ${Mixin.absoluteFill}
   overflow: hidden;
+  z-index: 0;
 `
 
 interface RippleEffectProps {
@@ -14,10 +15,11 @@ interface RippleEffectProps {
   radius: number
   cx: number
   cy: number
+  mount?: boolean
 }
 
 export const RippleEffect = styled("span")<RippleEffectProps>(
-  ({ theme, color, radius, cx, cy }) => css`
+  ({ theme, color, radius, cx, cy, mount = true }) => css`
     ${Mixin.absoluteFill}
     left: ${cx - radius}px;
     top: ${cy - radius}px;
@@ -25,19 +27,39 @@ export const RippleEffect = styled("span")<RippleEffectProps>(
     border-radius: ${radius}px;
     width: ${radius * 2}px;
     height: ${radius * 2}px;
-    animation: ${ripple} 400ms ease-out;
+    animation: ${scaleOut} 350ms ease-in-out, ${mount ? fadeIn : fadeOut} 350ms ease-in-out;
     animation-fill-mode: forwards;
   `
 )
 
-const ripple = keyframes`
+const scaleOut = keyframes`
   0% {
-    opacity: 1;
+    opacity: 0.7;
     transform: scale(0);
   }
 
   100% {
-    opacity: 0;
+    opacity: 1;
     transform: scale(1);
+  }
+`
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0.7;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+export const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
   }
 `
--- packages/crayon-ui/src/components/Ripple/Ripple.tsx
+++ packages/crayon-ui/src/components/Ripple/Ripple.tsx
@@ -1,9 +1,9 @@
-import { FC, memo, PointerEvent, useState } from "react"
+import { FC, memo, PointerEvent, useRef, useState, AnimationEvent } from "react"
 import { ColorVariant } from "@emotion/react"
 
 import { useMeasure } from "../../hooks"
 
-import { RippleEffect, RippleRoot } from "./Ripple.styled"
+import { fadeOut, RippleEffect, RippleRoot } from "./Ripple.styled"
 
 interface Props {
   color?: ColorVariant
@@ -11,13 +11,44 @@ interface Props {
 }
 
 export const Ripple: FC<Props> = memo(({ color = "primary", center }) => {
+  const currentRippleKey = useRef<string | null>()
   const { ref, rect } = useMeasure()
   const [effects, setEffects] = useState<JSX.Element[]>([])
 
-  const removeEffect = (key: string) => () =>
-    setEffects((prev) => prev.filter((effect) => effect.key !== key))
+  const removeRippleByKey = (key: string) => (e: AnimationEvent) => {
+    if (e.animationName === fadeOut.name) {
+      setEffects((prev) => prev.filter((effect) => effect.key !== key))
+    }
+  }
+
+  const hideEffect = () => {
+    if (!currentRippleKey.current) {
+      return
+    }
+    const key = currentRippleKey.current
+    currentRippleKey.current = null
+    setEffects((prev) =>
+      prev.map((ripple) => {
+        if (ripple.key !== key) {
+          return ripple
+        }
+        return (
+          <RippleEffect
+            key={key}
+            {...ripple.props}
+            mount={false}
+            onAnimationEnd={removeRippleByKey(key)}
+          />
+        )
+      })
+    )
+  }
 
   const showEffect = ({ clientX, clientY }: PointerEvent<HTMLSpanElement>) => {
+    if (currentRippleKey.current) {
+      hideEffect()
+    }
+
     const { left, top, width, height } = rect.current
     const cx = center ? width / 2 : clientX - left
     const cy = center ? height / 2 : clientY - top
@@ -25,22 +56,20 @@ export const Ripple: FC<Props> = memo(({ color = "primary", center }) => {
       Math.pow(Math.max(cx, width - cx), 2) + Math.pow(Math.max(cy, height - cy), 2)
     )
     const key = `${performance.now()}`
-
+    currentRippleKey.current = key
     setEffects((prev) => [
       ...prev,
-      <RippleEffect
-        key={key}
-        color={color}
-        radius={radius}
-        cx={cx}
-        cy={cy}
-        onAnimationEnd={removeEffect(key)}
-      />
+      <RippleEffect key={key} color={color} radius={radius} cx={cx} cy={cy} />
     ])
   }
 
   return (
-    <RippleRoot ref={ref} onPointerDown={showEffect}>
+    <RippleRoot
+      ref={ref}
+      onPointerDown={showEffect}
+      onPointerMove={hideEffect}
+      onPointerUp={hideEffect}
+    >
       {effects}
     </RippleRoot>
   )
