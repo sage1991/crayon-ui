cb10bc3f670d80ceafe2896f7d73982a0f60bf73
Harry <harry.kane@kakaostyle.com>
2022-09-24 00:29:45 +0900

    Forward reference to input

--- packages/crayon-ui/src/components/Checkbox/Checkbox.tsx
+++ packages/crayon-ui/src/components/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { FC, InputHTMLAttributes } from "react"
+import { forwardRef, InputHTMLAttributes } from "react"
 import { Interpolation } from "@emotion/serialize"
 import { ColorVariant, Theme } from "@emotion/react"
 
@@ -14,23 +14,24 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
   checkedIcon?: JSX.Element
 }
 
-export const Checkbox: FC<Props> = ({
-  css,
-  style,
-  className,
-  icon = <CheckBoxOutlineBlankRounded />,
-  checkedIcon = <CheckBoxRounded />,
-  disabled = false,
-  color = "primary",
-  checked,
-  ...props
-}) => {
+export const Checkbox = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const {
+    css,
+    style,
+    className,
+    icon = <CheckBoxOutlineBlankRounded />,
+    checkedIcon = <CheckBoxRounded />,
+    disabled = false,
+    color = "primary",
+    checked,
+    ...rest
+  } = props
   return (
     <CheckboxRoot css={css} style={style} className={className} disabled={disabled} color={color}>
       {checked ? checkedIcon : icon}
       <Ripple color={color} center>
-        <Input {...props} type="checkbox" />
+        <Input {...rest} ref={ref} type="checkbox" checked={checked} />
       </Ripple>
     </CheckboxRoot>
   )
-}
+})
