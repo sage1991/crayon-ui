35843194a2d2dc8a21009250813732e8508d011f
Harry <harry.kane@kakaostyle.com>
2022-09-24 23:56:26 +0900

    Fix alpha function bug

--- packages/crayon-ui/src/utils/color/toString.ts
+++ packages/crayon-ui/src/utils/color/toString.ts
@@ -6,11 +6,12 @@ export const toString = ({ type, value: { red, green, blue }, alpha }: ColorInfo
     const r = red.toString(16).padStart(2, "0")
     const g = green.toString(16).padStart(2, "0")
     const b = blue.toString(16).padStart(2, "0")
-    const a = alpha
-      ? clamp(round(alpha * 255), { maximum: 255, minimum: 0 })
-          .toString(16)
-          .padStart(2, "0")
-      : ""
+    const a =
+      typeof alpha === "number" && alpha < 1
+        ? clamp(round(alpha * 255), { maximum: 255, minimum: 0 })
+            .toString(16)
+            .padStart(2, "0")
+        : ""
     return `#${r}${g}${b}${a}`
   }
 
