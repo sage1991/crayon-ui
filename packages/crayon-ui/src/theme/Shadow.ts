6b3348b61dcc442a6be3ba59a65b634ee704bc0e
Harry <harry.kane@kakaostyle.com>
2022-09-24 22:28:32 +0900

    Change shadow value

--- packages/crayon-ui/src/theme/Shadow.ts
+++ packages/crayon-ui/src/theme/Shadow.ts
@@ -1,5 +1,8 @@
 export namespace Shadow {
-  export const low = "0 4px 4px rgba(0, 0, 0, 0.4)"
-  export const medium = "0 8px 8px rgba(0, 0, 0, 0.4)"
-  export const high = "0 12px 12px rgba(0, 0, 0, 0.4)"
+  export const low =
+    "0px 2px 1px -1px rgb(0, 0, 0, 0.2), 0px 1px 1px 0px rgb(0, 0, 0, 0.14), 0px 1px 3px 0px rgb(0, 0, 0, 0.12)"
+  export const medium =
+    "rgba(0, 0, 0, 0.2) 0px 3px 5px -1px, rgba(0, 0, 0, 0.14) 0px 5px 8px 0px, rgba(0, 0, 0, 0.12) 0px 1px 14px 0px"
+  export const high =
+    "rgba(0, 0, 0, 0.2) 0px 5px 6px -3px, rgba(0, 0, 0, 0.14) 0px 9px 12px 1px, rgba(0, 0, 0, 0.12) 0px 3px 16px 2px;"
 }
