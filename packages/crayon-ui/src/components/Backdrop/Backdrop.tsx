5ab4116131f47bb45a9439583294c4f2d7b5cec6
Harry <harry.kane@kakaostyle.com>
2022-09-27 18:16:55 +0900

    Replace transition to fade

--- packages/crayon-ui/src/components/Backdrop/Backdrop.tsx
+++ packages/crayon-ui/src/components/Backdrop/Backdrop.tsx
@@ -1,14 +1,15 @@
 import { FC, HTMLAttributes } from "react"
-import { Transition } from "react-transition-group"
 
-import { BackdropRoot, entering, exiting } from "./Backdrop.styled"
+import { Fade } from "../Fade"
+
+import { BackdropRoot } from "./Backdrop.styled"
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   open?: boolean
 }
 
 export const Backdrop: FC<Props> = ({ open = false, ...props }) => (
-  <Transition in={open} timeout={300} unmountOnExit>
-    {(state) => <BackdropRoot css={state.includes("enter") ? entering : exiting} {...props} />}
-  </Transition>
+  <Fade in={open} timeout={300} unmountOnExit>
+    <BackdropRoot {...props} />
+  </Fade>
 )
