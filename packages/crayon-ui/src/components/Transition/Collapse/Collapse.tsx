045eb64985853f6ea1bcf80b20f2953487c11621
Harry <harry.kane@kakaostyle.com>
2022-09-28 19:31:45 +0900

    Fix collapse bug

--- packages/crayon-ui/src/components/Transition/Collapse/Collapse.tsx
+++ packages/crayon-ui/src/components/Transition/Collapse/Collapse.tsx
@@ -1,32 +1,30 @@
 import { FC } from "react"
-import { css } from "@emotion/react"
 
 import { TweenTransition } from "../TweenTransition"
 import { TransitionProps } from "../types"
-
-const animation = {
-  begin: { height: 0 },
-  end: { height: 100 }
-}
+import { useMeasure } from "../../../hooks"
+import { css } from "@emotion/react"
 
 interface Props extends TransitionProps {}
 
-export const Collapse: FC<Props> = ({ minHeight, timeout, children, ...rest }) => (
-  <TweenTransition {...rest} {...animation} transition={`height ${timeout}ms`} timeout={timeout}>
-    <div
-      css={css`
-        display: inline-block;
-        overflow: hidden;
-      `}
+export const Collapse: FC<Props> = ({ minHeight, timeout, children, ...rest }) => {
+  const { ref, rect } = useMeasure<HTMLDivElement>()
+  return (
+    <TweenTransition
+      {...rest}
+      unmountOnExit={false}
+      begin={{ height: 0 }}
+      end={{ height: rect.current.height }}
+      transition={`height ${timeout}ms`}
+      timeout={timeout}
     >
       <div
         css={css`
-          width: 100%;
-          height: 100%;
+          overflow: hidden;
         `}
       >
-        {children}
+        <div ref={ref}>{children}</div>
       </div>
-    </div>
-  </TweenTransition>
-)
+    </TweenTransition>
+  )
+}
