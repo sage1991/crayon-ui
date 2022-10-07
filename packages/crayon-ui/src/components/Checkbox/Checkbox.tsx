d9486813809b7e4c4c592e9b940b2912751ed146
harry <shsage91@gmail.com>
2022-10-07 23:27:42 +0900

    Remove css props from checkbox component

--- packages/crayon-ui/src/components/Checkbox/Checkbox.tsx
+++ packages/crayon-ui/src/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,5 @@
 import { forwardRef, InputHTMLAttributes } from "react"
-import { Interpolation } from "@emotion/serialize"
-import { ColorVariant, Theme } from "@emotion/react"
+import { ColorVariant } from "@emotion/react"
 
 import { CheckBoxOutlineBlankRounded } from "../icons"
 import { Ripple } from "../Ripple"
@@ -9,14 +8,12 @@ import { AnimatedCheckBoxRounded, CheckboxRoot, Input } from "./Checkbox.styled"
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   color?: ColorVariant
-  css?: Interpolation<Theme>
   icon?: JSX.Element
   checkedIcon?: JSX.Element
 }
 
 export const Checkbox = forwardRef<HTMLInputElement, Props>((props, ref) => {
   const {
-    css,
     style,
     className,
     icon = <CheckBoxOutlineBlankRounded />,
@@ -27,7 +24,7 @@ export const Checkbox = forwardRef<HTMLInputElement, Props>((props, ref) => {
     ...rest
   } = props
   return (
-    <CheckboxRoot css={css} style={style} className={className} disabled={disabled} color={color}>
+    <CheckboxRoot style={style} className={className} disabled={disabled} color={color}>
       {checked ? checkedIcon : icon}
       <Ripple color={color} center>
         <Input {...rest} ref={ref} type="checkbox" checked={checked} />
