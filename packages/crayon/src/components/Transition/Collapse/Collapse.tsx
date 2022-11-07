5e1e22c84db055e8b1235c1d5cf0fb3088970a49
Harry <harry.kane@kakaostyle.com>
2022-11-07 21:17:21 +0900

    Remove redundant code

--- packages/crayon/src/components/Transition/Collapse/Collapse.tsx
+++ packages/crayon/src/components/Transition/Collapse/Collapse.tsx
@@ -7,36 +7,34 @@ import { useMeasure } from "../../../hooks"
 
 interface Props extends TransitionProps {}
 
-export const Collapse: FC<Props> = ({ timeout, children, unmountOnExit, in: isIn, ...rest }) => {
-  const { measure, rect } = useMeasure<HTMLDivElement>({}, [isIn])
+export const Collapse: FC<Props> = ({ timeout, children, in: isIn, ...rest }) => {
+  const { measure, rect } = useMeasure<HTMLDivElement>({}, [])
   return (
-    <Transition in={isIn} unmountOnExit={unmountOnExit} timeout={{ enter: 0, exit: timeout }}>
-      {(status) => {
-        return (
-          <Transition {...rest} in={status === "entered"} timeout={{ enter: timeout, exit: 0 }}>
-            {(_status) => {
-              let height: number | string = "0px"
-              if (_status === "entering" || _status === "exiting") {
-                height = `${rect().height}px`
-              }
-              if (_status === "entered") {
-                height = "auto"
-              }
-              return (
-                <div
-                  css={css`
-                    overflow: hidden;
-                    transition: height ${timeout}ms;
-                    height: ${height};
-                  `}
-                >
-                  <div {...measure()}>{children}</div>
-                </div>
-              )
-            }}
-          </Transition>
-        )
-      }}
+    <Transition in={isIn} timeout={{ enter: 0, exit: timeout }}>
+      {(status) => (
+        <Transition {...rest} in={status === "entered"} timeout={{ enter: timeout, exit: 0 }}>
+          {(_status) => {
+            let height: number | string = "0px"
+            if (_status === "entering" || _status === "exiting") {
+              height = `${rect().height}px`
+            }
+            if (_status === "entered") {
+              height = "auto"
+            }
+            return (
+              <div
+                css={css`
+                  overflow: hidden;
+                  transition: height ${timeout}ms;
+                  height: ${height};
+                `}
+              >
+                <div {...measure()}>{children}</div>
+              </div>
+            )
+          }}
+        </Transition>
+      )}
     </Transition>
   )
 }
