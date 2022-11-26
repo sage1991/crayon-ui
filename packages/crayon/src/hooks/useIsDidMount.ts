82c5add7babe0939daec7558f2a8deb7f08ff41c
harry <shsage91@gmail.com>
2022-11-27 00:03:00 +0900

    Remove redundant hook

--- packages/crayon/src/hooks/useIsDidMount.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react"
-
-export const useIsDidMount = () => {
-  const isDidMount = useRef<boolean>(false)
-  useEffect(() => {
-    queueMicrotask(() => (isDidMount.current = true))
-  }, [])
-  return isDidMount
-}
